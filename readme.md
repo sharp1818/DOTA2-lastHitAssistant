fa8cea9f391adfbf7c76e644ba880332cf5f730b
Sharp Santillán <adrian.shura18@gmail.com>
2026-03-08 19:37:40 -0500

    add train.py for RTX 5060

--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
+# GPU
+RTX 5060
+# PYTHON VERSION
+3.14.3
 # CREATE PROJECT WITH
 python -m venv venv
+# OR 
+py -3.14 -m venv venv
 # ACTIVATE WITH
 venv/Scripts/activate
+# UPDATE requeriments.txt
+python -m pip freeze > requirements.txt
--- train.py
+++ train.py
@@ -1,20 +1,24 @@
 from ultralytics import YOLO
 import torch
 
-device = 0 if torch.cuda.is_available() else "cpu"
-print(f"Usando dispositivo: {torch.cuda.get_device_name(0) if device == 0 else 'CPU'}")
+def main():
+    device = 0 if torch.cuda.is_available() else "cpu"
+    print(f"Usando dispositivo: {torch.cuda.get_device_name(0) if device == 0 else 'CPU'}")
 
-model = YOLO("yolo26n.pt")
+    model = YOLO("yolo26n.pt")
 
-train_results = model.train(
-    data="dataset.yaml",
-    epochs=100, 
-    imgsz=640, 
-    device=device, 
-    batch=16,
-    workers=4,
-)
+    train_results = model.train(
+        data="dataset.yaml",
+        epochs=100, 
+        imgsz=640, 
+        device=device, 
+        batch=16,
+        workers=4,
+    )
 
-metrics = model.val()
-path = model.export(format="onnx") 
-print(f"Modelo exportado en: {path}")
+    metrics = model.val()
+    path = model.export(format="onnx") 
+    print(f"Modelo exportado en: {path}")
+    
+if __name__ == '__main__':
+    main()
