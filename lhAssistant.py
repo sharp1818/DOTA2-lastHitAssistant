e83d5a2b0357643b656dbd039f7d6738a9a4488f
Sharp Santillán <adrian.shura18@gmail.com>
2026-03-09 02:11:44 -0500

    add percentage

--- lhAssistant.py
+++ lhAssistant.py
@@ -25,12 +25,42 @@ def main():
 
             if results[0].boxes is not None:
                 boxes_data = results[0].boxes
-            
+                            
                 for box in boxes_data:
                     x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
                     cls = int(box.cls[0])
-                    color = (0, 255, 0) if cls == 0 else (0, 0, 255)
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+
+                    mid_y = y1 + (y2 - y1) // 2
+                    linea_bgr = frame[mid_y:mid_y+1, x1:x2] 
+
+                    if linea_bgr.size > 0:
+                        hsv_linea = cv2.cvtColor(linea_bgr, cv2.COLOR_BGR2HSV)
+                        if cls == 0: # ALIADO
+                            mask_color = cv2.inRange(hsv_linea, np.array([35, 40, 40]), np.array([85, 255, 255]))
+                        else: # ENEMIGO
+                            m1 = cv2.inRange(hsv_linea, np.array([0, 50, 40]), np.array([10, 255, 255]))
+                            m2 = cv2.inRange(hsv_linea, np.array([160, 50, 40]), np.array([180, 255, 255]))
+                            mask_color = cv2.bitwise_or(m1, m2)
+
+                        mask_negro = cv2.inRange(hsv_linea, np.array([0, 0, 0]), np.array([180, 255, 50]))
+                        barra_total_mask = cv2.bitwise_or(mask_color, mask_negro)
+                        indices = np.where(barra_total_mask > 0)[1]
+
+                        if len(indices) > 0:
+                            inicio_real = indices[0]
+                            fin_real = indices[-1]
+                            ancho_real_barra = fin_real - inicio_real + 1
+
+                            pixeles_vida = np.count_nonzero(mask_color[0, inicio_real:fin_real+1])
+                            percentage = (pixeles_vida / ancho_real_barra) * 100
+                            if percentage > 95: percentage = 100
+                            else: percentage = int(percentage)
+
+                            # Visualización
+                            color_ui = (0, 255, 0) if cls == 0 else (0, 0, 255)
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), color_ui, 2)
+                            cv2.putText(frame, f"{percentage}%", (x1, y1 - 10), 
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_ui, 2)
             
             cv2.imshow("Dota 2 - RTX 5060 Vision", cv2.resize(frame, (1280, 720)))
             if cv2.waitKey(1) & 0xFF == ord('q'):
